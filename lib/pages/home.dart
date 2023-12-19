6e2e11e41beb6da97be10135ffdfb5a2281e3803
luddthree~ <luddetv@gmail.com>
2023-12-19 15:26:45 +0100

    start

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('home screen'),
+      body: SafeArea(child: Text('home screen')),
     );
   }
 }
