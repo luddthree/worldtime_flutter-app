155fcb168074209aa5da943830fc27005b1a6f83
luddthree~ <luddetv@gmail.com>
2023-12-28 16:07:07 +0100

    world time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,8 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -13,26 +10,16 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London'));
-    Map data = jsonDecode(response.body);
-    // print(data);
-
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-// print(datetime);
-// print(offset);
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
   }
 
   @override
   void initState( ) {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
 
---
+++ lib/services/world_time.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location;
+  String time;
+  String flag;
+  String url;
+
+  WorldTime({required this.location, required this.flag, required this.url}) : time = '';
+
+  Future<void> getTime() async {
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    time = now.toString();
+  }
+}
+
