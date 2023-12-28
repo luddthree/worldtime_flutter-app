e64c7de4fc38d15e34ec4024a5e6ec3bd5b6f60a
luddthree~ <luddetv@gmail.com>
2023-12-28 16:10:21 +0100

    loading time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,10 +10,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = 'loading';
+
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
@@ -26,7 +31,8 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(padding: EdgeInsets.all(50),
+      child: Text(time),)
     );
   }
 }
